a72ca208ea6f154f2c47a2f550651fd29e6bd333
Augusto Cattafesta <augustocattafesta@gmail.com>
2025-07-09 16:21:56 +0200

    added __init__.py

---
+++ hexrec/__init__.py
@@ -0,0 +1,14 @@
+"""Init constructor for package
+"""
+
+from pathlib import Path
+
+__pkgname__ = 'hexrec'
+
+HEXREC_ROOT = Path(__file__).parent
+HEXREC_BASE = HEXREC_ROOT.parent
+HEXREC_BIN = HEXREC_ROOT / 'bin'
+
+HEXREC_DATA = Path().home() / 'hexrecdata'
+if not Path.exists(HEXREC_DATA):
+    Path().mkdir(HEXREC_DATA)
