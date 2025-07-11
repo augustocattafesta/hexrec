1f0c6fb5f1511f392e57df902eb5ec50e9323b87
Augusto Cattafesta <augustocattafesta@gmail.com>
2025-07-11 15:11:58 +0200

    triangular beam added

--- hexrec/source.py
+++ hexrec/source.py
@@ -1,10 +1,54 @@
 """Extension of Source class from hexsample
 """
 
+from dataclasses import dataclass
+from typing import Tuple
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from hexsample.source import SpectrumBase
+from hexsample.source import BeamBase, SpectrumBase
+
+@dataclass
+class TriangularBeam(BeamBase):
+    """Triangular X-ray beam inside an hexagon 
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the center of the hexagon in cm.
+
+    y0 : float
+        The y-coordinate of the center of the hexagon in cm.
+
+    A : np.ndarray
+        The (x, y) coordinates of the first vertex of the hexagon in cm.
+
+    B : np.ndarray
+        The (x, y) coordinates of the second vertex of the hexagon in cm.
+    """
+
+    A_xy: tuple = (0, 0)
+    B_xy: tuple = (0, 0)
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        assert len(self.A_xy) == 2
+        assert len(self.B_xy) == 2
+        
+        A = np.array([[*self.A_xy]])
+        B = np.array([[*self.B_xy]])
+        C = np.array([[self.x0, self.y0]])
+
+        a = A - C
+        b = B - C
+
+        u = np.random.uniform(0, 1, (2, size))
+
+        mask = u[0, :] + u[1, :] > 1
+        u[:, mask] = 1 - u[:, mask]
+        w = np.dot(a.T, u[0, :, None].T) + np.dot(b.T, u[1, :, None].T)
+
+        return w[0] + C[0, 0], w[1] + C[0, 1]
 
 class Line(SpectrumBase):
     """Class describing a monochromatic emission line at a given
--- tests/test_source.py
+++ tests/test_source.py
@@ -5,7 +5,75 @@ import matplotlib.pyplot as plt
 
 from loguru import logger
 
-from hexrec.source import Line
+from hexrec.source import Line, TriangularBeam
+
+from hexsample.hist import Histogram1d, Histogram2d
+from hexsample.plot import setup_gca
+from hexsample.hexagon import HexagonalGrid, HexagonalLayout
+from hexsample.display import HexagonalGridDisplay
+
+def test_triangular_beam(num_photons = 1000000):
+    """Test for TriangularBeam class
+
+    Args:
+        size (int, optional): _description_. Defaults to 10000.
+    """
+    beam = TriangularBeam(0, 0, (0, 1), (1, 0))
+
+    x, y = beam.rvs(num_photons)
+    binning_x = np.linspace(min(x), max(x), 100)
+    binning_y = np.linspace(min(y), max(y), 100)
+    plt.figure('Traingular beam')
+    Histogram2d(binning_x, binning_y).fill(x, y).plot()
+    setup_gca(xlabel='x [cm]', ylabel='y [cm]')
+    plt.figure('Triangular beam x projection')
+    hx = Histogram1d(binning_x).fill(x)
+    hx.plot()
+    plt.figure('Triangular beam x projection')
+    hy = Histogram1d(binning_y).fill(y)
+    hy.plot()
+
+def test_triangular_beam_grid(num_photons = 100000):
+    """Test for TrinagularBeam on hexagonal grid
+
+    Args:
+        num_photons (int, optional): _description_. Defaults to 100000.
+    """
+    target_col = 5
+    target_row = 6
+    layout = HexagonalLayout('ODD_Q')
+    pitch = 0.005
+    grid = HexagonalGrid(layout, 10, 8, pitch)
+    x0, y0 = grid.pixel_to_world(target_col, target_row)
+    logger.debug(f'Grid size -> {10, 8}')
+    logger.debug(f'Target hexagon -> {target_col, target_row}')
+    logger.debug(f'Target hexagon center -> {x0, y0}')
+    display = HexagonalGridDisplay(grid)
+    display.draw(pixel_labels=True)
+    plt.plot(x0, y0, '+b')
+
+    # Find vertex
+    l = pitch / 2   # Not sure about this value, maybe different between layouts
+    if grid.pointy_topped():
+        A_xy = (x0 + l*np.sqrt(3)/2, y0 - l/2)
+        B_xy = (x0 + l*np.sqrt(3)/2, y0 + l/2)
+    else:
+        A_xy = (x0 + l, y0)
+        B_xy = (x0 + l/2, y0 + l*np.sqrt(3)/2)
+
+    beam = TriangularBeam(x0, y0, A_xy, B_xy)
+    x, y = beam.rvs(num_photons)
+    plt.scatter(x, y, color='red', s=0.1)
+    display.setup_gca()
+
+
+    col, row = grid.world_to_pixel(x, y)
+    pixel = np.array([col, row]).T
+    pixel_vals = np.unique(pixel, return_counts=True, axis=0)
+
+    logger.debug(f'Total number of photons: {num_photons}')
+    for p_val, p_counts in zip(*pixel_vals):
+        logger.debug(f'Pixel {tuple(p_val)} hit {p_counts}')
 
 def test_line(energy = 6000, size = 100000):
     """Test for line class
@@ -30,6 +98,8 @@ def test_line(energy = 6000, size = 100000):
     line.plot()
 
 if __name__ == '__main__':
-    test_line(energy=7000)
+    # test_triangular_beam()
+    test_triangular_beam_grid()
+    # test_line(energy=7000)
 
     plt.show()
