5301734a592c015ace9bdcaf6c9d463420bbfc7d
Augusto Cattafesta <augustocattafesta@gmail.com>
2025-07-17 17:03:32 +0200

    two pixels events

---
+++ hexrec/clustering.py
@@ -0,0 +1,115 @@
+"""Extension of clustering.py from hexsample
+"""
+
+# Definire la funzione di fit e un metodo per valutarla in funzione dei primi
+# due valori di pha
+
+from dataclasses import dataclass
+from typing import Tuple
+
+import numpy as np
+
+from hexsample.digi import DigiEventSparse, DigiEventCircular, DigiEventRectangular
+from hexsample.readout import HexagonalReadoutCircular
+from hexsample.clustering import Cluster, ClusteringBase
+from hexsample.modeling import PowerLaw
+
+GAMMA_BARYCENTER1D = 0.267
+
+class Cluster(Cluster):
+    def __init__(self, x: np.ndarray, y: np.ndarray, pha: np.ndarray) -> None:
+        super().__init__(x, y, pha)
+
+    def barycenter_1d(self, pitch) -> Tuple[float, float]:
+        # Va bene solo se i due pixel sono su asse x
+        if not self.x.shape[0] == 2:
+            raise RuntimeError(f'Clusters must contain only 2 pixels')
+        
+        # Bisogna gestire meglio questa cosa, perchè se va più carica nel pixel
+        # adiacente quando vado a sommare la posizione è di quella adiacente e non
+        # quella centrale
+
+        # inoltre non va bene per tutte le orientazioni degli esagoni e dei triangoli
+        x0 = 0.0012499999999999734
+        if self.x[0] == x0:
+            eta = self.pha[1] / self.pulse_height()
+            x = PowerLaw().eval(eta/0.5, 0.5, GAMMA_BARYCENTER1D)*pitch + self.x[0]
+        else:
+            eta = self.pha[0] / self.pulse_height()
+            x = PowerLaw().eval(eta/0.5, 0.5, GAMMA_BARYCENTER1D)*pitch + self.x[1]
+
+        return x, self.y[0]
+        
+
+@dataclass
+class ClusteringNN(ClusteringBase):
+
+    """Neirest neighbor clustering.
+
+    This is a very simple clustering strategy where we use the highest pixel in
+    the event as a seed, loop over the six neighbors (after the zero suppression)
+    and keep the N highest pixels.
+
+    Arguments
+    ---------
+    num_neighbors : int
+        The number of neighbors (between 0 and 6) to include in the cluster.
+    """
+
+    num_neighbors: int
+
+    def run(self, event) -> Cluster:
+        """Overladed method.
+
+        .. warning::
+           The loop ever the neighbors might likely be vectorized and streamlined
+           for speed using proper numpy array for the offset indexes.
+        """
+        if isinstance(event, DigiEventSparse):
+            pass
+        elif isinstance(event, DigiEventCircular):
+            # If the readout is circular, we want to take all the neirest neighbors.
+            self.num_neighbors = HexagonalReadoutCircular.NUM_PIXELS - 1 # -1 is bc the central px is already considered
+            col = [event.column]
+            row = [event.row]
+            adc_channel_order = [self.grid.adc_channel(event.column, event.row)]
+            # Taking the NN in logical coordinates ...
+            for _col, _row in self.grid.neighbors(event.column, event.row):
+                col.append(_col)
+                row.append(_row)
+                # ... transforming the coordinates of the NN in its corresponding ADC channel ...
+                adc_channel_order.append(self.grid.adc_channel(_col, _row))
+            # ... reordering the pha array for the correspondance (col[i], row[i]) with pha[i].
+            pha = event.pha[adc_channel_order]
+            # Converting lists into numpy arrays
+            col = np.array(col)
+            row = np.array(row)
+            pha = np.array(pha)
+        # pylint: disable = invalid-name
+        elif isinstance(event, DigiEventRectangular):
+            seed_col, seed_row = event.highest_pixel()
+            col = [seed_col]
+            row = [seed_row]
+            for _col, _row in self.grid.neighbors(seed_col, seed_row):
+                col.append(_col)
+                row.append(_row)
+            col = np.array(col)
+            row = np.array(row)
+            pha = np.array([event(_col, _row) for _col, _row in zip(col, row)])
+        # Zero suppressing the event (whatever the readout type)...
+        pha = self.zero_suppress(pha)
+        # Array indexes in order of decreasing pha---note that we use -pha to
+        # trick argsort into sorting values in decreasing order.
+        idx = np.argsort(-pha)
+        # Only pick the seed and the N highest pixels.
+        # This is useless for the circular readout because in that case all 
+        # neighbors are used for track reconstruction.
+        mask = idx[:self.num_neighbors + 1]
+        # If there's any zero left in the target pixels, get rid of it.
+        mask = mask[pha[mask] > 0]
+        # Trim the relevant arrays.
+        col = col[mask]
+        row = row[mask]
+        pha = pha[mask]
+        x, y = self.grid.pixel_to_world(col, row)
+        return Cluster(x, y, pha)
--- scripts/reconstruction.py
+++ scripts/reconstruction.py
@@ -5,11 +5,14 @@ import argparse
 from tqdm import tqdm
 
 from hexsample import logger
-from hexsample.clustering import ClusteringNN
+# from hexsample.clustering import ClusteringNN
 from hexsample.readout import HexagonalReadoutCircular
 from hexsample.fileio import DigiInputFileCircular, ReconOutputFile
 from hexsample.hexagon import HexagonalLayout
 from hexsample.recon import ReconEvent
+
+from hexrec.recon import ReconEvent1DBary
+from hexrec.clustering import ClusteringNN
 
 __description__ = \
 """Run the reconstruction on a file produced by hxsim.py
@@ -17,9 +20,11 @@ __description__ = \
 
 parser = argparse.ArgumentParser(description=__description__)
 parser.add_argument('infile', help='Input file path')
+parser.add_argument('--rcmethod', choices=['centroid', 'fit'], type=str, 
+                    default='centroid', help='How to reconstruct position')
 parser.add_argument('--zsupthreshold', default=0, type=float,
                      help='zero suppresion threshold in ADC counts (default: 0)')
-parser.add_argument('--nneighbors', default=6, type=int, 
+parser.add_argument('--nneighbors', default=6, type=int,
                     help='number of neighbors to be considered (default: 6)')
 parser.add_argument('--suffix', default='recon', type=str,
                     help='suffix for the output file (default: recon)')
@@ -29,6 +34,7 @@ def hxrecon(args):
     """Application main entry point.
     """
     input_file_path = args.infile
+    rcmethod = args.rcmethod
     kwargs = {}
     kwargs['zsupthreshold'] = args.zsupthreshold
     kwargs['nneighbors'] = args.nneighbors
@@ -48,12 +54,18 @@ def hxrecon(args):
     output_file = ReconOutputFile(output_file_path)
     output_file.update_header(**kwargs)
     output_file.update_digi_header(**input_file.header)
+
     for i, event in tqdm(enumerate(input_file)):
         cluster = clustering.run(event)
-        args = event.trigger_id, event.timestamp(), event.livetime, cluster
-        recon_event = ReconEvent(*args)
-        mc_event = input_file.mc_event(i)
-        output_file.add_row(recon_event, mc_event)
+        if cluster.size() == 2:
+            args = event.trigger_id, event.timestamp(), event.livetime, cluster
+            if rcmethod == 'centroid':
+                recon_event = ReconEvent(*args)
+            elif rcmethod == 'fit':
+                recon_event = ReconEvent1DBary(*args)
+            mc_event = input_file.mc_event(i)
+            output_file.add_row(recon_event, mc_event)
+
     output_file.flush()
     input_file.close()
     output_file.close()
